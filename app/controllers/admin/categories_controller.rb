79fb820fe985461f7c607a057ca8145cfe95fbb8
Stanley <Stanley741025@gmail.com>
2018-05-29 17:28:04 +0800

    add form on index page for creating new category. And add error msg notice.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -7,6 +7,25 @@ class Admin::CategoriesController < ApplicationController
     #@page = params[:page] || 1
     #@restaurants=Restaurant.order("updated_at DESC").page(params[:page]).per(10)
     @categories = Category.page(params[:page]).per(10)
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "category was successfully created"
+      redirect_to admin_categories_path
+    else
+      flash[:alert] = "category was failed to create"
+      @categories = Category.all.page(params[:page]).per(10)
+      render :index
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:id, :name)
   end
 
 end
