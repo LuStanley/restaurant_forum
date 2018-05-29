67b6647d6ffe2cb86c855cde40f3287fcb14cc3e
Stanley <Stanley741025@gmail.com>
2018-05-29 16:23:47 +0800

    add category index for admin

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,5 +2,11 @@ class Admin::CategoriesController < ApplicationController
 
   before_action :authenticate_user!
   before_action :authenticate_admin
-  
+
+  def index
+    #@page = params[:page] || 1
+    #@restaurants=Restaurant.order("updated_at DESC").page(params[:page]).per(10)
+    @categories = Category.page(params[:page]).per(10)
+  end
+
 end
