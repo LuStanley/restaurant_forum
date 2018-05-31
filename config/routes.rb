e86c21676902e654c1bf04ea801a2e29c5035521
Stanley <Stanley741025@gmail.com>
2018-05-31 22:49:17 +0800

    setup comment nested resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,14 @@ Rails.application.routes.draw do
   root "restaurants#index"
   resources :restaurants, only: [:index, :show]
   resources :categories, only: :show
+
+  resources :restaurants, only: [:index, :show] do
+    resources :comments, only: [:create, :destroy]
+  end
+
   namespace :admin do
     resources :restaurants
     resources :categories
-
     root "restaurants#index"
   end
 end
