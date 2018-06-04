f7bf547a9a0d5a728345e47a58117f689de21691
Stanley <Stanley741025@gmail.com>
2018-06-04 15:04:52 +0800

    setup user routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root "restaurants#index"
   resources :restaurants, only: [:index, :show]
   resources :categories, only: :show
+  resources :users, only: [:show, :edit, :update]
 
   resources :restaurants, only: [:index, :show] do
     resources :comments, only: [:create, :destroy]
