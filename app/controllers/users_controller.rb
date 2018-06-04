b5477b54da320826a9b69d84b7d65dd8fc0ad972
Stanley <Stanley741025@gmail.com>
2018-06-04 23:05:46 +0800

    add user edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(@user)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :intro, :avatar)
+  end
 end
