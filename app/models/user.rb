306e3525ef1c31decdef8096ebacbd2abe2d3380
Stanley <Stanley741025@gmail.com>
2018-06-05 17:00:49 +0800

    Remove all user show page. And add has_many restaurant in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :comments
+  has_many :restaurant, through: :comments
 
   mount_uploader :avatar, AvatarUploader
 
